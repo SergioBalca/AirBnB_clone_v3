1c913b84ea04f2b8f742a5f9d2f3ff2205b33afd
SergioBalca <3896@holbertonschool.com>
2022-05-30 14:18:39 -0500

    self passed as argument to close method in teardown_appcontext

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,6 @@ from os import getenv
 from threading import Thread
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,7 +12,7 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext():
+def close(self):
     """ method to handle teardown_appcontext
         that calls storage.close()
     """
@@ -25,7 +24,4 @@ if __name__ == "__main__":
     hbnb_api_host = getenv("HBNB_API_HOST")
     hbnb_api_port = getenv("HBNB_API_PORT")
 
-    if hbnb_api_host and hbnb_api_port:
-        app.run(host=hbnb_api_host, port=hbnb_api_port, threaded=True)
-    else:
-        app.run(host="0.0.0.0", port=5000, threaded=True)
+    app.run(host=hbnb_api_host, port=hbnb_api_port, threaded=True)
