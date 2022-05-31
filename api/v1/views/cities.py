63394ba999380db480d409ed3dca4fc36f072528
fyga10 <frankgiraldo62@outlook.com>
2022-05-31 17:28:59 -0500

    import fixed

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,7 +6,6 @@ from models.base_model import *
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-from models.city import *
 from flask import jsonify, abort, request, make_response
 
 
