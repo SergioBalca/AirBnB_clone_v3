5b9c5a51e82e1080e08978e83bc41dc364a7c47c
SergioBalca <3896@holbertonschool.com>
2022-05-31 17:52:11 -0500

    new view for Amenity objects that handles all default RESTFul API actions

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ View for Amenity objects """
 
-from crypt import methods
 from models.base_model import *
 from api.v1.views import app_views
 from models import storage
@@ -10,13 +9,13 @@ from flask import jsonify, abort, request, make_response
 
 
 @app_views.route('/amenities', strict_slashes=False, methods=['GET', 'POST'])
-def get_amenities():
-    """Function that retrieve and save a new amenities"""
+def amenities():
+    """Function that retrieve and save a new amenity"""
     la = []
     amenities = storage.all('Amenity').values()
     if request.method == "GET":
         for amenity in amenities:
-            la.append(amenities.to_dict())
+            la.append(amenity.to_dict())
         return jsonify(la)
     elif request.method == "POST":
         if not request.json:
@@ -32,7 +31,7 @@ def get_amenities():
 @app_views.route('/amenities/<amenity_id>', strict_slashes=False,
                  methods=['GET', 'DELETE', 'PUT'])
 def amenity(amenity_id):
-    """Function that retrieve, delete and put a Amenity"""
+    """Function that retrieve, delete and put an amenity"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity:
         if request.method == "GET":
@@ -40,7 +39,7 @@ def amenity(amenity_id):
         elif request.method == "DELETE":
             storage.delete(amenity)
             storage.save()
-            return make_response(jsonify({}), 200)
+            return {}
         elif request.method == "PUT":
             if not request.json:
                 return make_response(jsonify({'error': "Not a JSON"}), 400)
@@ -53,5 +52,4 @@ def amenity(amenity_id):
                 amenity.updated_at = datetime.utcnow()
                 storage.save()
                 return make_response(amenity.to_dict(), 200)
-    """If amenity_id is not linked to a Amenity object"""
     abort(404)
