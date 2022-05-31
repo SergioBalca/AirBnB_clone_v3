13f6d35d1365ec0ad9275c7011f4cb2a17ae82ee
SergioBalca <3896@holbertonschool.com>
2022-05-31 17:10:42 -0500

    new view for City objects that handles all default RESTFul API actions

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,33 +12,36 @@ from flask import jsonify, abort, request, make_response
 @app_views.route('/states/<state_id>/cities', strict_slashes=False,
                  methods=['GET', 'POST'])
 def cities_state(state_id):
-    """Retrieves a city object"""
+    """Function that retrieve and save a new City object"""
     state = storage.get(State, state_id)
     cities = storage.all('City').values()
-    cities_lists = []
+    cities_list = []
     if state:
         for city in cities:
-            cities_lists.append(city.to_dict())
+            if city.state_id == state_id:
+                cities_list.append(city.to_dict())
         if request.method == "GET":
-            return jsonify(cities_lists)
+            return jsonify(cities_list)
         elif request.method == "POST":
             if not request.json:
-                return make_response(jsonify({'error': "Not a JSON"}), 400)
-        elif 'name' not in request.json:
-            return make_response(jsonify({'error': "Missing name"}), 400)
-        else:
-            r_json = request.json
-            r_json['state_id'] = state_id
-            new = City(**r_json)
-            new.save()
-            return make_response(new.to_dict(), 201)
+                return make_response(jsonify(
+                                     {'error': "Not a JSON"}), 400)
+            elif 'name' not in request.json:
+                return make_response(jsonify(
+                                     {'error': "Missing name"}), 400)
+            else:
+                r_json = request.json
+                r_json['state_id'] = state_id
+                new = City(**r_json)
+                new.save()
+                return make_response(new.to_dict(), 201)
     abort(404)
 
 
 @app_views.route('/cities/<city_id>', strict_slashes=False,
                  methods=['GET', 'DELETE', 'PUT'])
 def city(city_id):
-    """function that retrieves, deletes and put a city"""
+    """Function that retrieve, delete and put a City"""
     city = storage.get(City, city_id)
     if city:
         if request.method == "GET":
@@ -46,17 +49,17 @@ def city(city_id):
         elif request.method == "DELETE":
             storage.delete(city)
             storage.save()
-            return make_response(jsonify({}), 200)
+            return {}
         elif request.method == "PUT":
             if not request.json:
                 return make_response(jsonify({'error': "Not a JSON"}), 400)
             else:
-                json = request.json
-            for key, value in json.items():
-                if key != 'id' and key != 'state_id' and\
-                   key != 'created_at' and key != "updated_at":
-                    setattr(city, key, value)
-            city.updated_at = datetime.utcnow()
-            storage.save()
-            return make_response(city.to_dict(), 200)
+                r_json = request.json
+                for key, value in r_json.items():
+                    if key != 'id' and key != 'state_id' and\
+                       key != 'created_at' and key != "updated_at":
+                        setattr(city, key, value)
+                city.updated_at = datetime.utcnow()
+                storage.save()
+                return make_response(city.to_dict(), 200)
     abort(404)
